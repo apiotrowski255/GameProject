eeb390e0ebd43bb78c743fe2e66564d376fb409e
Alex Piotrowski <awesomea900@gmail.com>
2016-11-06 17:47:31 +1100

    Create Player.java

---
+++ src/data/Player.java
@@ -0,0 +1,46 @@
+package data;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import static helpers.Artist.*;
+
+public class Player {
+
+	private TileGrid grid;
+	private TileType[] types;
+	private int index;
+	
+	
+	public Player(TileGrid grid){
+		this.grid = grid;
+		this.types = new TileType[3];
+		this.types[0] = TileType.Grass;
+		this.types[1] = TileType.Dirt;
+		this.types[2] = TileType.Water;
+		this.index = 0;
+	}
+	
+	public void setTile(){
+		grid.SetTile((int)Math.floor(Mouse.getX() / 64), (int)Math.floor((HEIGHT - Mouse.getY() - 1)/64), types[index]);
+	}
+	
+	public void update(){
+		if (Mouse.isButtonDown(0)){
+			setTile();
+		}
+		
+		while (Keyboard.next()){
+			if(Keyboard.getEventKey() == Keyboard.KEY_RIGHT && Keyboard.getEventKeyState()){
+				System.out.println("right");
+				MoveIndex();
+			}
+		}
+	}
+	
+	public void MoveIndex(){
+		index ++;
+		if(index > types.length - 1) {
+			index = 0;
+		}
+	}
+}
